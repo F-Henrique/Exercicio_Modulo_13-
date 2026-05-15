643a5f368cbedafc8b88bf82dfd6a21c7ceea270
F-Henrique <santos.rique89@gmail.com>
2026-05-15 01:20:22 -0300

    adicionando classes do exercicio do módulo 13

---
+++ Principal.java
@@ -0,0 +1,35 @@
+package exercicios;
+
+import abstratas.Empregado;
+import abstratas.Horista;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+    PessoaFisica pessoa= new PessoaFisica();
+    pessoa.setNome("Fulano de Tal");
+    pessoa.setCpf("123.456.789-01");
+    imprimir(pessoa);
+
+    //############################
+    System.out.println("");
+    //############################
+
+    PessoaJuridica empresa =new PessoaJuridica();
+    empresa.setNome("Fulano de tal LTDA");
+    empresa.setCnpj("33.134.369/0001-46");
+    imprimir(empresa);
+
+    }
+
+    public static void imprimir(Pessoa pessoa) {
+        if (pessoa instanceof PessoaJuridica) {
+            System.out.println("Empresa: '"+pessoa.getNome() + "', CNPJ de número: " + pessoa.identificacao());
+        }else{
+            System.out.println("Cidadão: '"+pessoa.getNome() + "', CPF de número: " + pessoa.identificacao());
+        }
+
+    }
+
+}
